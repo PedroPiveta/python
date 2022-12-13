8d61f5468ec50ed84988bd686ed993056912c632
PedroPiveta <ppivetabarrotti208@gmail.com>
2022-12-13 16:02:58 -0300

    sequences

---
+++ sequences.py
@@ -0,0 +1,7 @@
+#list - mutable values
+#tuple - immutable values
+#set - unordered collection of unique values
+#dictionary - unordered collection of key-value pairs
+
+coordinate = (10.0, 20.0) # Tuple
+
