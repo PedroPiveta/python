5869df6300511905e07f85ee048322bc7e9c6a01
PedroPiveta <ppivetabarrotti208@gmail.com>
2022-12-13 16:06:25 -0300

    lists

---
+++ lists.py
@@ -0,0 +1,12 @@
+# Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print(names[0])
+
+# Adds a new name at the end of list
+names.append("Draco")
+
+# Sorts the list in alphabetical order
+names.sort()
+
+print(names)
